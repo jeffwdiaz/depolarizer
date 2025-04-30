e0f5bbb3c80799a40481ccb7162e94631426bcdf
Jeff <jeffwdiaz@gmail.com>
2025-04-29 22:12:53 -0400

    Restructure: separate frontend and backend, update .gitignore, add backend skeleton

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.90xooRMu.js","_app/immutable/chunks/CWj6FrbW.js","_app/immutable/chunks/69_IOA4Y.js","_app/immutable/chunks/DIeogL5L.js","_app/immutable/chunks/Ca8lzTS7.js","_app/immutable/chunks/CT4Je91J.js","_app/immutable/chunks/NP7W43fV.js","_app/immutable/chunks/c52YGuGB.js"];
-export const stylesheets = [];
-export const fonts = [];
